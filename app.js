1ee17b728379a2fd54184e354ccfabbe75294ffa
Arturo Grottoli <vamosdavid@gmail.com>
2021-10-29 14:36:52 -0300

    Solucion error formulario

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ function scrollTopButton(btn){
 }
 
 
-
-
-
-
-
 //Funcion mostrar mensaje
 
 
@@ -35,27 +30,3 @@ let mostrarMensaje = () =>{
 }
 
 
-/*Funcion Formulario*/
-const $form = document.querySelector('#form')
-$form.addEventListener('submit', handleSubmit)
-
-async function handleSubmit(event) {
-  event.preventDefault()
-  const form = new FormData(this)
-  const response = await fetch(this.action, {
-    method: this.method,
-    body:form,
-    headers:{
-      'Accept': 'application/json'      }
-  })
-
-  if(response.ok){
-    this.reset()
-    swal({
-      title: "Tu mensaje fue enviado!",
-      text: "Gracias por contactarte, nos comunicaremos con vos lo mas pronto posible.",
-      icon: "success",
-    });
-    
-  }
-}
